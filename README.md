977fa2f767d53de9c88d91ceb83bf213c06c3c6e
Med Hacen <hacen13037@gmail.com>
2022-02-12 20:52:49 +0300

    Adding comments

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 # Convert PDF file to Text file
-This simple script asks you for the path to a pdf and then converts this given pdf to
-a text file and store the result to the given output path or create a folder `\temp` 
-and store the text file in it if no output path is provided.
+This simple script asks the user for the path to a pdf file and then converts this given pdf to
+a text file and store the result to a given output path or create a new folder `\temp` 
+and store the text file their if no output path is provided.
 
-### Library used 
+### Libraries used 
 - PyPDF2
 - OS
 
--- pdf_text_converter.py
+++ pdf_text_converter.py
@@ -1,26 +1,32 @@
 import PyPDF2
 import os
 
+# Initialize the paths
 textPath = ""
 pdfPath = ""
 
-pdfPath = input("Enter the path of your pdf file: ")
+# Get the the source path and the output path
+pdfPath = input("Enter the path to your pdf file: ")
 textPath = input("(Optional) Enter the path to save the text file in: ")
 
-if (len(textPath) == 0):
-    if (os.path.isdir("temp") == False):
+# Create the default path in case when the output path is not provided
+if (len(textPath) == 0): # Check if the output path is given or not
+    if (os.path.isdir("temp") == False): # Check if the temp folder is already exist or not
         os.mkdir("temp")
     BASEDIR = os.path.realpath("temp")
     textPath = os.path.join(BASEDIR, os.path.basename(os.path.normpath(pdfPath)).replace(".pdf", "")+".txt")
     print("The text file is saved to the folder: ", BASEDIR)
 
+# Open and read the pdf file
 pdfObj = open(pdfPath, 'rb')
 pdfRead = PyPDF2.PdfFileReader(pdfObj)
 
-x = pdfRead.numPages
-for i in range(x):
+# Copy the content of each page in the pdf file to the text file
+pages = pdfRead.numPages
+for i in range(pages):
     pageObj = pdfRead.getPage(i)
     with open(textPath, 'a+') as f:
         f.write((pageObj.extractText()))
 
+# Close the pdf file
 pdfObj.close()
